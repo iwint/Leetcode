7bedc8d9907a941b95fb1bd86ff5f1b1e10adaea
iwint <iwint.cseasnsct@gmail.com>
2024-07-06 23:08:28 +0530

    solved fibnocci series using recursion

---
+++ src/Recursion/Fibonacci.java
@@ -0,0 +1,15 @@
+package Recursion;
+
+public class Fibonacci {
+
+
+    public int getNthTerm(int n) {
+
+        if (n < 2) {
+            return n;
+        }
+        return getNthTerm(n - 1) + getNthTerm(n - 2);
+    }
+
+
+}
---
+++ src/Recursion/Main.java
@@ -0,0 +1,9 @@
+package Recursion;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Fibonacci fibonacci = new Fibonacci();
+        System.out.println(fibonacci.getNthTerm(7));
+    }
+}
