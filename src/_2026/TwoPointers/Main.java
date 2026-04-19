817c1ae373322e49dba9902e66032fbbb6a116cd
iwint <iwint.cseasnsct@gmail.com>
2026-04-19 20:34:14 +0530

    Day 2: Two Pointers Learning

--- src/_2026/TwoPointers/Main.java
+++ src/_2026/TwoPointers/Main.java
@@ -7,8 +7,14 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a target:\t");
         int target = scanner.nextInt();
-        int[] nums = new int[]{1, 2, 3, 5, 6};
+        int[] nums = new int[]{1, 2, 5, 5, 6};
+
+        //Two Sum
         TwoSum twoSum = new TwoSum(target, nums);
         System.out.println(twoSum.isTwoSumExist());
+
+        //Remove Occurrences
+        RemoveOccurrences removeOccurrences = new RemoveOccurrences(nums, target);
+        System.out.println(removeOccurrences.getUniqueElementsCount());
     }
 }
