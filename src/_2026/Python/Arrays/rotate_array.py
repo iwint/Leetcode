196c5abe4aab8ca865815884ec4abe8eede92d73
iwint <iwint.cseasnsct@gmail.com>
2026-08-02 14:12:13 +0530

    [02-08-2026] - Solved rotate array problem

---
+++ src/_2026/Python/Arrays/rotate_array.py
@@ -0,0 +1,17 @@
+def rotate_array(nums: list[int], k: int) -> None:
+    """First Approach"""
+    # temp = nums[(len(nums) - k):] + nums[:len(nums) - k]
+    # for i in range(len(temp)):
+    #     nums[i] = temp[i]
+    """Second Approach"""
+    # nums.reverse()
+    # for i in range(k):
+    #     temp = nums[0]
+    #     nums.remove(temp)
+    #     nums.append(temp)
+    #
+    # nums.reverse()
+    print(nums)
+
+
+rotate_array([-1, -100, 3, 99], 2)
