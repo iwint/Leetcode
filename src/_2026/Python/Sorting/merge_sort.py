43d0f91c3d84867c7523e8471b5c792698632e40
iwint <iwint.cseasnsct@gmail.com>
2026-08-11 22:50:22 +0530

    [11-08-2026] - Learned merge sort algorithm

--- src/_2026/Python/Sorting/merge_sort.py
+++ src/_2026/Python/Sorting/merge_sort.py
@@ -1,10 +1,45 @@
-def merge_sort():
-    return
+def merge_sort(arr: list[int], left: int, right: int):
+    if left >= right:
+        return
+
+    mid = left + (right - left) // 2
+
+    merge_sort(arr, left, mid)
+    merge_sort(arr, mid + 1, right)
+    merge(arr, left, mid, right)
 
 
-def merge():
-    return
+def merge(arr: list[int], left: int, mid: int, right: int):
+    left_arr = arr[left: mid + 1]
+    right_arr = arr[mid + 1: right + 1]
+
+    i = 0
+    j = 0
+    k = left
+
+    while i < len(left_arr) and j < len(right_arr):
+
+        if left_arr[i] <= right_arr[j]:
+            arr[k] = left_arr[i]
+            i += 1
+        else:
+            arr[k] = right_arr[j]
+            j += 1
+
+        k += 1
+
+    while i < len(left_arr):
+        arr[k] = left_arr[i]
+        i += 1
+        k += 1
+
+    while j < len(right_arr):
+        arr[k] = right_arr[j]
+        j += 1
+        k += 1
 
 
-def run():
-    return
+if __name__ == "__main__":
+    arr = [38, 27, 43, 3, 9, 82, 10]
+    merge_sort(arr, 0, len(arr) - 1)
+    print(arr)
