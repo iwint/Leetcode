41f4ba05868a07a64d206b91fac568b9bbf93813
Iwin Issac <74755442+iwint@users.noreply.github.com>
2026-09-01 16:17:52 +0530

    [01/09/2026] : Solved reverse pairs using merge sort

--- src/_2026/Python/Sorting/reverse_pairs.py
+++ src/_2026/Python/Sorting/reverse_pairs.py
@@ -11,4 +11,65 @@ def reverse_pairs(nums: list[int]):
     return counter
 
 
-print(reverse_pairs(nums=[1, 3, 2, 3, 1]))
+def reverse_pairs_using_merge_sort(nums: list[int], l: int, h: int):
+
+    if l >= h:
+        return 0
+
+    mid = l + (h - l) // 2
+    count = 0
+    count += reverse_pairs_using_merge_sort(nums, l, mid)
+    count += reverse_pairs_using_merge_sort(nums, mid + 1, h)
+    count += find_count(nums, l, mid, h)
+    merge(nums, l, mid, h)
+    return count
+
+
+def merge(nums: list[int], l: int, mid: int, h: int):
+
+    left_arr = nums[l : mid + 1]
+    right_arr = nums[mid + 1 : h + 1]
+
+    i = 0
+    j = 0
+    k = l
+
+    while i < len(left_arr) and j < len(right_arr):
+        if left_arr[i] < right_arr[j]:
+            nums[k] = left_arr[i]
+            i += 1
+        else:
+            nums[k] = right_arr[j]
+            j += 1
+        k += 1
+
+    while i < len(left_arr):
+        nums[k] = left_arr[i]
+        i += 1
+        k += 1
+
+    while j < len(right_arr):
+        nums[k] = right_arr[j]
+        j += 1
+        k += 1
+
+
+def find_count(nums: list[int], l: int, mid: int, h: int):
+
+    i = l
+    j = mid + 1
+    count = 0
+
+    while i <= mid and j <= h:
+        if nums[i] > (2 * nums[j]):
+            count += mid - i + 1
+            j += 1
+        else:
+            i += 1
+
+    return count
+
+
+nums = [1, 3, 2, 3, 1]
+
+print(reverse_pairs_using_merge_sort(nums, 0, len(nums) - 1))
